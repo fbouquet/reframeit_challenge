4addc93f8fa4d095df6051a429f30f57fd04724a
Florent Bouquet <florent.b.bouquet@orange.fr>
2014-02-16 17:52:11 +0100

    Init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReframeitChallenge::Application.config.secret_key_base = '71bdd0b95c19ad3db45bbabba4d6afea99e55a13702975344c5d403f7687b820b4594dec499244d14bf47d418d19e4f5b68320e6af978320ee84f8c97e9a9882'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReframeitChallenge::Application.config.session_store :cookie_store, key: '_reframeit_challenge_session'
