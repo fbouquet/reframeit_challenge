5bec4169982ab97fa126cbdd8f387da42f8a387c
Florent Bouquet <florent.b.bouquet@orange.fr>
2014-02-18 21:46:35 +0100

    Finish users: making it look better, edit and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   	end
 
 	def index
-		@users = User.all
+		@users = User.paginate(page: params[:page])
 	end
 
 	def new
@@ -15,14 +15,43 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 		  sign_in @user
-		  flash[:success] = "Successfully signed up."
+		  flash[:success] = "Welcome, " + @user.first_name + " " + @user.name + "!"
 		  redirect_to @user
 		else
 		  render 'new'
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params_update)
+			flash[:success] = "Successfully changed parameters."
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		if @user.destroy
+			flash[:success] = "Successfully deleted user."
+			redirect_to root_path
+		else
+			redirect_to @user
+		end
+	end
+
+
 	def user_params
     	params.require(:user).permit(:first_name, :name, :email, :influency, :password, :password_confirmation)
   	end
-end
+
+  	def user_params_update
+    	params.require(:user).permit(:influency, :password, :password_confirmation)
+  	end
+end	
