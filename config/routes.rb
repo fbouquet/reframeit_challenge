ec15954b1de0f7fc38a033faf7e1f9e811c8c8b1
Florent Bouquet <florent.b.bouquet@orange.fr>
2014-02-19 14:34:56 +0100

    Add static home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ReframeitChallenge::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-  root to: "users#index"
+  root to: "static_pages#home"
   match '/signup', to: 'users#new', via: "get"
   match '/signin',  to: 'sessions#new', via: "get"
   match '/signout', to: 'sessions#destroy', via: "get"
